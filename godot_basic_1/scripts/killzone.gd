1b3992c34ea53fcce6958c4376f6795ecb78d8d4
Darknight <hiep.lchan@gmail.com>
2026-01-11 11:09:13 +0700

    add AnimationPlayer to play coin SFX

--- godot_basic_1/scripts/killzone.gd
+++ godot_basic_1/scripts/killzone.gd
@@ -1,9 +1,11 @@
 extends Area2D
 
 @onready var timer: Timer = $Timer
+@onready var die_sfx: AudioStreamPlayer2D = $DieSFX
 
 func _on_body_entered(body: Node2D) -> void:
 	print("You died!!!")
+	die_sfx.play()
 	Engine.time_scale = 0.2
 	body.get_node("CollisionShape2D").queue_free()
 	timer.start()
--- godot_basic_1/scripts/player.gd
+++ godot_basic_1/scripts/player.gd
@@ -5,6 +5,7 @@ const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var jump_sfx: AudioStreamPlayer2D = $JumpSFX
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -14,6 +15,7 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		jump_sfx.play()
 
 	# Get direction: -1, 0, 1
 	var direction := Input.get_axis("move_left", "move_right")
